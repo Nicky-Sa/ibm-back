df93006e8eaba3b091870c43cad86b90b6032e82
nicki-di <kimjonghyun.jhk59@gmail.com>
2022-03-10 22:24:12 +0330

    some config changes

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ let config = {}
 
 config.db = {
     "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "CC1",
-    "port": 3307
+    "user": "admin_CC1",
+    "password": "dbpassword",
+    "database": "admin_CC1",
+    "port": 3306
 }
 
 module.exports = config;
